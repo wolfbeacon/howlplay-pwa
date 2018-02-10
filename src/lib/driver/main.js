00bda60524353b15d21fbec99ee5a12f89b7408c
Jun Zheng <junzheng@exchange.niyume.com>
2018-02-09 20:25:11 -0500

    Added basic driver, created pingHandler (#4)

---
+++ src/lib/driver/main.js
@@ -0,0 +1,70 @@
+let Driver = {
+    handlers: {
+        /**
+         * Handle ping event
+         * @param connection
+         * @param data
+         * @param storage
+         */
+        pingHandler: function(connection, data, storage){
+            return Promise((resolve, reject) => {
+                resolve(Driver.buildPayload(0));
+            });
+        }
+    },
+    emitters: {},
+
+    // internal utilities
+    _util: {
+        /**
+         * Build payload to be sent by WebSocket
+         * @param opcode
+         * @param payload
+         * @returns {*}
+         */
+        buildPayload: function(opcode, payload = ""){
+            let operation = new Uint8Array([opcode]);
+            let newPayload = new Uint8Array(this.stringToArrayBuffer(payload));
+            return this.concatTypedArrays(operation, newPayload).buffer;
+        },
+
+        /**
+         * Convert a string to ArrayBuffer using our custom SINU encoder
+         * https://wolfbeacon.atlassian.net/wiki/spaces/WG/pages/409534465/HowlPlay+WebSocket+Sub-Protocol+Specification
+         * @param str
+         * @returns {ArrayBuffer}
+         */
+        stringToArrayBuffer: function(str){
+            let buf = new ArrayBuffer(str.length);
+            let bufView = new Uint8Array(buf);
+            for (let i = 0, strLen = str.length; i < strLen; i++) {
+                bufView[i] = str.charCodeAt(i);
+            }
+            return buf;
+        },
+
+        /**
+         * Convert an ArrayBuffer back to string
+         * @param buf
+         * @returns {string}
+         */
+        arrayBufferToString: function(buf){
+            return String.fromCharCode.apply(null, new Uint8Array(buf));
+        },
+
+        /**
+         * Concat two typed arrays, and returns array in type of 'a'
+         * @param a
+         * @param b
+         * @returns {*}
+         */
+        concatTypedArrays: function(a, b) { // a, b TypedArray of same type
+            let c = new (a.constructor)(a.length + b.length);
+            c.set(a, 0);
+            c.set(b, a.length);
+            return c;
+        }
+    }
+};
+
+module.exports = Driver;
