a2c53c966ac794dabb0b360caebaa281b00b2c02
nafees <nafees.rawji@mail.utoronto.ca>
2018-04-02 16:36:31 -0400

    Fixed minor bugs

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -3,6 +3,7 @@ import Question from '../components/gamepage/Question'
 import {connect} from 'react-redux';
 import {bindActionCreators} from "redux";
 import {getQuizData} from "../redux/actions/gameServerActions";
+import {push} from "react-router-redux";
 
 
 
@@ -10,6 +11,9 @@ class GamePage extends Component {
 
     componentDidMount(){
         this.props.getQuizData();
+        if (!this.props.gameServerLink) {
+            this.props.push('/');
+        }
     }
 
     render() {
@@ -18,7 +22,7 @@ class GamePage extends Component {
                 <div id="question-box">
                     <Question build={this.props.quizData[0]}/>
                     <p id="question-left"><span id="question-left-count">5</span> questions left</p>
-                    <p>Connected to server: { this.props.gameServerLink }</p>
+                    <p>Connected to server: {(this.props.socket) ? this.props.gameServerLink : null}</p>
                     <p>Using nickname: { this.props.nickname }</p>
                 </div>
             </section>
@@ -31,9 +35,10 @@ class GamePage extends Component {
 const mapStateToProps = state => {
     return {gameServerLink: state.gameServer.link,
     nickname: state.gameServer.nickname,
-    quizData: state.gameServer.quizData}
+    quizData: state.gameServer.quizData,
+    socket: state.socket !== null}
 };
 
-const mapDispatchToProps = dispatch => bindActionCreators({getQuizData}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({getQuizData, push}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(GamePage);
--- src/redux/reducers/gameServerReducer.js
+++ src/redux/reducers/gameServerReducer.js
@@ -1,7 +1,7 @@
 import {SET_QUIZ_DATA, GAME_SERVER_INPUT_ERROR, SET_GAME_SERVER} from "../actions/gameServerActions";
 
 const initialState = {
-    gameServerLink: "",
+    link: "",
     nickname: "",
     error: null,
     quizData: null
@@ -12,7 +12,7 @@ function gameServerReducer(state = initialState, action){
 
     switch (action.type) {
         case SET_GAME_SERVER:
-            return {...state, gameServerLink: action.payload.link,
+            return {...state, link: action.payload.link,
                 nickname: action.payload.nickname};
         case GAME_SERVER_INPUT_ERROR:
             return {...state, error: action.error};
