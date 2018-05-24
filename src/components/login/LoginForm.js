7040c9e5d4bfbfcfeb3a6d7f2128970c7c0a5efc
Frederic Pun <frederic.pun@gmail.com>
2018-05-24 00:03:07 -0400

    Added more error messages"

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -48,19 +48,20 @@ class LoginForm extends Component {
                             <label className="login-form-label sr-only" htmlFor="code-input">Server Code</label>
                             <input type="text" className="login-input" id="code-input" name="code"
                                    onChange={this.onTextChange.bind(this)} placeholder="X8UKL"/>
-                            {this.props.type === "QUIZCODE" ? <label id="login-error-area">{this.props.error}</label> : null}
+                            {this.props.type === "QUIZCODE" || this.props.type === "QUIZCODE" ? <label className="login-error-area">{this.props.error}</label> : null}
                         </div>
                         <div className="login-input-section">
                             <label className="login-form-label sr-only" htmlFor="nickname-input">Nickname</label>
                             <input type="text" className="login-input" id="nickname-input" name="nickname"
                                    onChange={this.onTextChange.bind(this)} placeholder="Nickname"/>
-                            {this.props.type === "NICKNAME" ? <label id="login-error-area">{this.props.error}</label> : null}
+                            {this.props.type === "NICKNAME" ? <label className="login-error-area">{this.props.error}</label> : null}
                         </div>
                     </div>
                 </div>
                 <button type="button" id="login-submit-button"
                         onClick={() => {this.props.checkAndSwitchToGamePage(this.state);console.log(this.props);} }>Login
                 </button>
+                <p className="login-error-area" style={{paddingTop: '14px'}}>{this.props.type === "SERVER" ? this.props.error : ""}</p>
             </form>
         );
     }
